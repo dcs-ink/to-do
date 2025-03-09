44f2d725e2a35fdcce4716000a0c88cf1c97bd47
Dan <dan.csharp@gmail.com>
2025-03-09 11:29:44 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   // Replace with your Firebase config object
-  apiKey: "AIzaSyDLn5xP2pxG9e5Mvphg53FQzPINLQ-1ngg",
-  authDomain: "todo-7952f.firebaseapp.com",
-  projectId: "todo-7952f",
-  storageBucket: "todo-7952f.firebasestorage.app",
-  messagingSenderId: "342170847837",
-  appId: "1:342170847837:web:31c4145becdafcf2cf5bb4"
+
 };
 
 const app = initializeApp(firebaseConfig);
